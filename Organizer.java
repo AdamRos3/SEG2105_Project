17e51c2f51382676c8964d888f86022301ed755b
Kblac0 <109545427+Kblac0@users.noreply.github.com>
2025-05-22 14:56:30 -0400

    Revert "Uploading files from previous repo"
    This reverts commit 9a2c514af55ea4f71dbf4a06d6f24cd33b930b77.

--- Organizer.java
+++
@@ -1,6 +0,0 @@
-public class Organizer extends UserAccount {
-    public Organizer(String inputtedUsername, String inputtedPassword) {
-        super(inputtedUsername, inputtedPassword);
-        typeOfAccount = "Organizer";
-    }
-}
--- Participant.java
+++
@@ -1,6 +0,0 @@
-public class Participant extends UserAccount {
-    public Participant(String inputtedUsername, String inputtedPassword) {
-        super(inputtedUsername, inputtedPassword);
-        typeOfAccount = "Participant";
-    }
-}
